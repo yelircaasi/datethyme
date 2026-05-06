04c00b38983cd9d5b00986f45805fb54e6b69dd3
yelircaasi <yelircaasi@proton.me>
2026-05-06 07:08:49 +0200

    add notes for display

---
+++ src/datethyme/scheduling/display.py
@@ -0,0 +1,13 @@
+"""
+Streak (for habits)
+Github-style activity level view
+colored calendar block view, like calendar.calendar(2026)
+
+use calendar.HTMLCalendar output as reference for my own HTML
+
+circular/tall-rectangular time use aggregation (by context? -> support passing a mapper)
+"""
+
+from calendar import calendar
+
+calendar(2026, m=3)
